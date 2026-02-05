7ba16933ee4ca0d21b35f909869808675c544468
erningflores <146033433+erningflores@users.noreply.github.com>
2026-02-05 09:16:42 +0800

    added my new logic

--- within_repo/src/main.rs
+++ within_repo/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
     println!("Hello, world!");
+
+    let x: i32 = -40;
+    let y: u64 = 100;
+    println!("Signed integer: {}", x);
+    println!("Unsigned integer: {}", y);
+
+    let a1: char = 'A';
+    println!("Letter: {}", a1);
+
+    let b1: bool = true;
+    println!("Is it snowing: {}", b1);
+
+    //slice
+    let name: &str = "Jacob";
+    println!("My name is {}", name);
+
+    let f1: f32 = 3.14;
+    println!("Pi is {}", f1);
 }
