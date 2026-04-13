7e51ad76a386b721051de96daef4f73b49bedcaa
erningflores <146033433+erningflores@users.noreply.github.com>
2026-04-13 09:22:37 +0800

    Lifetime in trait

---
+++ rust_example66/src/main.rs
@@ -0,0 +1,19 @@
+#[derive(Debug)]
+struct Borrowed<'a>{
+    x: &'a i32,
+}
+
+impl<'a> Default for Borrowed<'a>{
+    fn default() -> Self{
+        Self{
+            x: &10,
+        }
+    }
+}
+
+fn main() {
+    println!("Lifetime in traits!\n");
+
+    let b: Borrowed = Default::default();
+    println!("b is {:?}", b);
+}
